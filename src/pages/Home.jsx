43e84262c53f1c5b3420c9feb20e72bb945961f4
patr4519 <jero96@mail.ru>
2023-09-05 01:49:25 +0300

    Rewriten layout home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import { fetchPosts, fetchTags, fetchComments } from "../redux/slices/posts";
 import { Link, useLocation } from "react-router-dom";
 import { SideCommentsBlock } from "../components/SideCommentsBlock";
 import { List, ListItemButton, ListItemText } from "@mui/material";
+import Box from "@mui/material/Box";
+import Masonry from "@mui/lab/Masonry";
 
 const navigationItems = [
   { to: "/news", label: "Новые" },
@@ -37,65 +39,65 @@ export const Home = () => {
   }, [location]);
 
   return (
-    <>
-      <List style={{ display: "flex", flexDirection: "row" }}>
-        {navigationItems.map((item, index) => (
-          <Link
-            key={index}
-            to={item.to}
-            style={{
-              textDecoration: "none",
-              color: "black",
-            }}
-          >
-            <ListItemButton>
-              <ListItemText primary={item.label} />
-            </ListItemButton>
-          </Link>
-        ))}
-      </List>
-      <Grid container spacing={4}>
-        <Grid xs={8} item>
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "repeat(2, 1fr)",
-              gap: "16px",
-            }}
-          >
-            {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
-              isPostsLoading ? (
-                <Post key={index} isLoading={true} />
-              ) : (
-                <Post
-                  key={index}
-                  id={obj._id}
-                  title={obj.title}
-                  text={obj.text}
-                  imageUrl={
-                    obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ""
-                  }
-                  user={obj.user}
-                  createdAt={obj.createdAt}
-                  viewsCount={obj.viewsCount}
-                  commentsCount={obj.comments.length}
-                  tags={obj.tags}
-                  likes={obj.likes.count}
-                  userLikes={obj.likes.users}
-                  isEditable={userData?._id === obj.user._id}
-                />
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={8}>
+        <List style={{ display: "flex", flexDirection: "row" }}>
+          {navigationItems.map((item, index) => (
+            <Link
+              key={index}
+              to={item.to}
+              style={{
+                textDecoration: "none",
+                color: "black",
+              }}
+            >
+              <ListItemButton>
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            </Link>
+          ))}
+        </List>
+        <Box sx={{ minHeight: 829 }}>
+          <Masonry columns={2} spacing={2}>
+            {(isPostsLoading ? [...Array(5)] : posts.items).map(
+              (obj, index) => (
+                <div key={index}>
+                  {isPostsLoading ? (
+                    <Post key={index} isLoading={true} />
+                  ) : (
+                    <Post
+                      key={index}
+                      id={obj._id}
+                      title={obj.title}
+                      text={obj.text}
+                      imageUrl={
+                        obj.imageUrl
+                          ? `http://localhost:4444${obj.imageUrl}`
+                          : ""
+                      }
+                      user={obj.user}
+                      createdAt={obj.createdAt}
+                      viewsCount={obj.viewsCount}
+                      commentsCount={obj.comments.length}
+                      tags={obj.tags}
+                      likes={obj.likes.count}
+                      userLikes={obj.likes.users}
+                      isEditable={userData?._id === obj.user._id}
+                    />
+                  )}
+                </div>
               )
             )}
-          </div>
-        </Grid>
-        <Grid xs={4} item>
-          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
-          <SideCommentsBlock
-            items={comments.items}
-            isLoading={isCommentsLoading}
-          />
-        </Grid>
+          </Masonry>
+        </Box>
       </Grid>
-    </>
+      <Grid sx={{ marginTop: 8 }} item xs={12} sm={4}>
+        <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+        <SideCommentsBlock
+          items={comments.items}
+          isLoading={isCommentsLoading}
+        />
+      </Grid>
+    </Grid>
   );
 };
